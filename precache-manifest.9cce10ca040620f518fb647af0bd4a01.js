8f041dad480a6abb08d2f0d5ce20021c64557fbe
harryli0088 <harryli0088@gmail.com>
2020-09-06 16:44:42 -0400

    Updates

---
+++ precache-manifest.9cce10ca040620f518fb647af0bd4a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ce9e0073ad0ff787c5fbb9bbdc30cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d7ffa5268598a2882fc",
+    "url": "/static/css/main.a496d1cc.chunk.css"
+  },
+  {
+    "revision": "3dc60a863f166fb6e91a",
+    "url": "/static/js/2.d834c2be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d834c2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7ffa5268598a2882fc",
+    "url": "/static/js/main.d638e7fb.chunk.js"
+  },
+  {
+    "revision": "adcc79f4fda0e935f3a0",
+    "url": "/static/js/runtime-main.039d5c7d.js"
+  }
+]);
